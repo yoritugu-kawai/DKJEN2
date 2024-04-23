bd9a49a1bf0d4ebfd25f951175cc04e6db35d625
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-04-23 23:20:20 +0900

    1

--- DKJEN/PostEffect/OffScreenRendering/RenderTextrure.h
+++ DKJEN/PostEffect/OffScreenRendering/RenderTextrure.h
@@ -5,7 +5,6 @@ class RenderTextrure
 {
 public:
 	void Initialize();
-	ComPtr<ID3D12Resource> CreateRenderTextrureResource(ComPtr<ID3D12Device> device,uint32_t width ,uint32_t height, DXGI_FORMAT format ,const Vector4& clearClolor);
+	ComPtr<ID3D12Resource> CreateRenderTextrureResource(ComPtr<ID3D12Device> device, uint32_t width, uint32_t height, DXGI_FORMAT format, const Vector4& clearClolor);
 private:
-
 };
