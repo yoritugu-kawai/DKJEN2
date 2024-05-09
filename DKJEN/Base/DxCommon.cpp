de9aa4b157d933cd469624ae25b44a6aff823830
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-05-09 10:17:20 +0900

    1

--- DKJEN/Base/DxCommon.cpp
+++ DKJEN/Base/DxCommon.cpp
@@ -306,12 +306,12 @@ void DxCommon::CreateRTV()
 		D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	 device->CreateRenderTargetView(renderTextureResource.Get(), &rtvDesc, rtvHandles[2]);
 	//SRV
-	D3D12_SHADER_RESOURCE_VIEW_DESC renderTextureSrvDesc{};
+	/*D3D12_SHADER_RESOURCE_VIEW_DESC renderTextureSrvDesc{};
 	renderTextureSrvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
 	renderTextureSrvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 	renderTextureSrvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
 	renderTextureSrvDesc.Texture2D.MipLevels = 1;
-	device->CreateShaderResourceView(renderTextureResource.Get(), &renderTextureSrvDesc, rtvHandles[3]);
+	device->CreateShaderResourceView(renderTextureResource.Get(), &renderTextureSrvDesc, rtvHandles[3]);*/
 
 	DxCommon::GetInstance()->rtvDesc = rtvDesc;
 	DxCommon::GetInstance()->device = device;
