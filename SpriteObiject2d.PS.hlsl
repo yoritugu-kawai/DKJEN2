ac6638c5b5f1feabbcc437b0456a1db6ed9c82f0
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-06-11 18:24:07 +0900

    1

--- SpriteObiject2d.PS.hlsl
+++ SpriteObiject2d.PS.hlsl
@@ -11,19 +11,26 @@ struct PixelShaderOutput {
 	float32_t4 color : SV_TARGET0;
 };
 
-static const float32_t3 kIndex3x3[3][3] =
-{
-    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
-    { { -1.0f, .0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
-    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, -1.0f } },
-};
-static const float32_t3 kKernal3x3[3][3] =
-{
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-};
+//static const float32_t3 kIndex3x3[3][3] =
+//{
+//    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+//    { { -1.0f, .0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
+//    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, -1.0f } },
+//};
+//static const float32_t3 kKernal3x3[3][3] =
+//{
+//    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+//    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+//    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+//};
 
+float gauss(float x, float y,float sigma){
+    static const float32_t PI = 3.14159265f;
+    float exponent = -(x * x + y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    return exp(exponent) * rcp(denominator);
+
+}
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
@@ -43,23 +50,37 @@ PixelShaderOutput main(VertexShaderOutput input)
     //vignette = saturate(pow(vignette, 0.8f));
     //output.color.rgb *= vignette;
     //CG502
-    uimt32_t widht, height;
-    gTexture.GetDimensions(widht, height);
-    float32_t uvStepSize = float32_t(rcp(widht), rcp(height));
+    //uimt32_t widht, height;
+    //gTexture.GetDimensions(widht, height);
+    //float32_t uvStepSize = float32_t(rcp(widht), rcp(height));
     
-    output.color.rgb = flost32_t3(0.0f, 0.0f, 0.0f);
-    output.color.a = 1.0f;
+    //output.color.rgb = flost32_t3(0.0f, 0.0f, 0.0f);
+    //output.color.a = 1.0f;
+    //for (int32_t x = 0; x < 3; ++x)
+    //{
+    //    for (int32_t y = 0; y < 3; ++y)
+    //    {
+    //        float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+    //        float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+    //        output.color.rgb += fetchColor * kKernal3x3[x][y];
+
+    //    }
+
+    //}
+    //CG503
+    float32_t wight = 0.0f;
+    float32_t kerne3x3[3][3];
     for (int32_t x = 0; x < 3; ++x)
     {
         for (int32_t y = 0; y < 3; ++y)
         {
-            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
-            float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            output.color.rgb += fetchColor * kKernal3x3[x][y];
+            kerne3x3[x][y] = gauss(kerne3x3[x][y].x, kerne3x3[x][y].y, 2.0f);
+            wight += kerne3x3[x][y];
 
         }
 
     }
+    output.color.rgb *= rcp(wight);
 	//output.color = gMaterial.color;
-    return output;
+        return output;
 }
