aa259058988d71ce05e1b77c6947ff31afd19a61
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-07-11 11:55:07 +0900

    1

--- DKJEN/PostEffect/PostEffect.cpp
+++ DKJEN/PostEffect/PostEffect.cpp
@@ -2,6 +2,7 @@
 
 void PostEffect::Initialize()
 {
+	Create();
 }
 
 void PostEffect::Call()
@@ -11,6 +12,6 @@ void PostEffect::Call()
 
 	commandList->SetGraphicsRootSignature(pso_.rootSignature);
 	commandList->SetPipelineState(pso_.GraphicsPipelineState);//
-	DescriptorManagement::rootParamerterCommand(2, tex_);
+	DescriptorManagement::rootParamerterCommand(2, srv_);
 	commandList->DrawInstanced(3, 1, 0, 0);
 }
--- DKJEN/PostEffect/PostEffect.h
+++ DKJEN/PostEffect/PostEffect.h
@@ -2,12 +2,22 @@
 #include"../Base/DxCommon.h"
 #include"../Base/RenderTextrurePSO.h"
 #include"../Management/DescriptorManagement.h"
+#include"../Utilipy/Pch.h"
 class PostEffect
 {
 public:
 	void Initialize();
 	void Call();
+protected:
+     void Create() {
+		// SRV作成
+		ComPtr<ID3D12Resource> stv =
+			RTVManager::GetRTV("PostEffect")->GetRTVPrope().Resources.Get();
+		srv_ = SRVManager::CreatePostEffectSRV(stv);
 
+		// バッファー作成
+		mtlBuffer_.CreateResource();
+	}
 private:
-
+	uint32_t srv_;
 };
