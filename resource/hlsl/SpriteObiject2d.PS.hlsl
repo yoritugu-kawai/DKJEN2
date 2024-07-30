049731f269b5fb687dcd015bca40810dfa6b82c7
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-07-30 14:36:22 +0900

    1

--- resource/hlsl/SpriteObiject2d.PS.hlsl
+++ resource/hlsl/SpriteObiject2d.PS.hlsl
@@ -20,8 +20,8 @@ PixelShaderOutput main(VertexShaderOutput input) {
 
 	output.color = gMaterial.color * textureColor;
     output.color = gTexture.Sample(gSampler, input.texcoord);
-    float32_t value  = dot(output.color.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
-    output.color.rgb = float32_t3(value, value, value);
+    //float32_t value  = dot(output.color.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    //output.color.rgb = float32_t3(value, value, value);
 	//output.color = gMaterial.color;
 	//CG501
     //output.color = gMaterial.color * textureColor;
