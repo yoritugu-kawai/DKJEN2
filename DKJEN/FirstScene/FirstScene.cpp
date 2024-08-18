5730818a6302613c739733333d5cceb10cf50cb8
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-08-18 23:01:40 +0900

    1

--- DKJEN/FirstScene/FirstScene.cpp
+++ DKJEN/FirstScene/FirstScene.cpp
@@ -3,7 +3,7 @@
 void FirstScene::Initialize()
 {
 	time = 12;
-	uint32_t tex = TexManager::LoadTexture("resource/DKJEN/DKJEN.png");
+	uint32_t tex = TexManager::LoadTexture("GameResource/black.png");
 	sprite = new Sprite;
 	sprite->Initialize(tex);
 	color = { 1.0f,1.0f,1.0f,1.0f };
@@ -14,13 +14,20 @@ void FirstScene::Update(GameManager* gameManager)
 
 
 
-	///color.w -= 0.1f;
-	if (color.w < 0) {
+	//color.w -= 0.1f;
+#ifdef _DEBUG
+	ImGui::Begin("Color");
+	ImGui::DragFloat4("c", &color.x, 0.1f, -1.0f, 1.0f);
+	
+
+	ImGui::End();
+#endif // _DEBUG
+	/*if (color.w < 0) {
 		
 			gameManager->ChangeState(new StartSeen);
 
 		
-	}
+	}*/
 
 }
 
